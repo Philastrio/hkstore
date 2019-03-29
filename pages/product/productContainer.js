afaeba1f2d8f4bf32260ad664f637f4d24b5ccfc
philastrio <han_gang@naer.com>
2019-03-30 05:42:08 +0900

    graphql fragment

---
+++ pages/product/productContainer.js
@@ -0,0 +1,10 @@
+import ProductPresenter from "./productPresenter";
+import { withRouter } from "next/router";
+
+class ProductContaier extends React.Component {
+  render() {
+    return <ProductContaier />;
+  }
+}
+
+export default withRouter(ProductContaier);
