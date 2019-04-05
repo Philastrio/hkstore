5bd61e58cc1486b8c9ca8c857c04e9aec9bbacc7
KANG <rio790@empas.com>
2019-04-05 10:44:15 +0900

    pwa_manifest

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,7 @@ export default class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.8.1/antd.min.css"
             rel="stylesheet"
           />
+          <link rel="manifest" href="/static/manifest.json" />
           <style>{`body { background-color: #EFF2F5!important}`}</style>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="black" />
