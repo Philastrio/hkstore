e13a98be25e7f6df09b5ea4d347a1845d67738b4
KANG <rio790@empas.com>
2019-04-06 14:14:43 +0900

    server

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,18 @@
-self.addEventListener('install');
+self.addEventListener("install", event => {
+  const offlinePage = new Request("/");
+  event.waitUntil(
+    fetch(offlinePage).then(response => {
+      return caches.open("hkstore").then(cache => {
+        return cache.put(offlinePage, response);
+      });
+    })
+  );
+});
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => console.log('Sorry we are offline'))
+    fetch(event.request).catch(error => {
+      return caches.open("hkstore").then(cache => cache.match("/"));
+    })
   );
 });
