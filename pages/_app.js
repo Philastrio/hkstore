e44c2d0bae6ada304315f199bba7bda1f1ae9a0c
philastrio <han_gang@naver.com>
2019-04-07 10:15:28 +0900

    push message 하다가 말았음 ㅠㅠ

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
-import { Layout } from "antd";
-import App, { Container } from "next/app";
-import React from "react";
-import withApollo from "../lib/withApollo";
-import { ApolloProvider } from "react-apollo";
-import withNProgress from "next-nprogress";
-import NProgress from "next-nprogress/component";
+import { Layout } from 'antd';
+
+import App, { Container } from 'next/app';
+
+import React from 'react';
+
+import withApollo from '../lib/withApollo';
+
+import { ApolloProvider } from 'react-apollo';
+
+import withNProgress from 'next-nprogress';
+
+import NProgress from 'next-nprogress/component';
+
+import convertDataURIToBinary from '../lib/base64';
 
 const msDelay = 1000;
 
@@ -17,30 +25,45 @@ class MyApp extends App {
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
+
     return { pageProps };
   }
 
   componentDidMount() {
-    if ("serviceWorker" in navigator && "PushManager" in window) {
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
       navigator.serviceWorker
-        .register("/sw.js")
+
+        .register('/sw.js')
+
         .then(swReg => {
-          console.log("SW Registered: ", swReg);
+          console.log('SW Registered: ', swReg);
+
           Notification.requestPermission().then(permission => {
-            if (permission === "granted") {
-              swReg.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: 
-              })
+            if (permission === 'granted') {
+              swReg.pushManager
+
+                .subscribe({
+                  userVisibleOnly: true,
+
+                  applicationServerKey: convertDataURIToBinary(
+                    'BEV4IChUXRTC-Og6VYt0bgJQa1LKCWYaHC8byp5hRUdm91fePLJZKgKK3te5mWoJcTgUUwg3t-DvrGb--36-v6M'
+                  )
+                })
+
+                .then(PushSubscriptionObject => {
+                  console.log(PushSubscriptionObject);
+                });
             }
           });
         })
+
         .catch(error => console.log("Can't register SW: ", error));
     }
   }
 
   render() {
     const { Component, pageProps, apollo } = this.props;
+
     return (
       <Container>
         <NProgress
@@ -49,9 +72,11 @@ class MyApp extends App {
           showAfterMs={300}
           spinner={true}
         />
+
         <ApolloProvider client={apollo}>
           <Layout>
             <Component {...pageProps} />
+
             <Footer>
               충청남도 홍성군 홍북읍 | 이아롬 | 사업자번호: 302-43-345112
             </Footer>
--- static/service-worker.js
+++ static/service-worker.js
@@ -1,18 +1,22 @@
-self.addEventListener("install", event => {
-  const offlinePage = new Request("/");
+self.addEventListener('install', event => {
+  const offlinePage = new Request('/');
   event.waitUntil(
     fetch(offlinePage).then(response => {
-      return caches.open("hkstore").then(cache => {
+      return caches.open('hkstore').then(cache => {
         return cache.put(offlinePage, response);
       });
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(error => {
-      return caches.open("hkstore").then(cache => cache.match("/"));
+      return caches.open('hkstore').then(cache => cache.match('/'));
     })
   );
+});
+
+self.addEventListener('push', event => {
+  console.log(event);
 });
